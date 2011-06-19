2534380a1577f6cd8f0340aa2da0a35dfd52474e
Arne Wischer <Fettlaus@gmail.com>
2011-06-19 18:13:36 +0000

    added bare webservice-functions

--- Messwerterfassung/src/sensor/Sensor.java
+++ Messwerterfassung/src/sensor/Sensor.java
@@ -1,11 +1,23 @@
 package sensor;
 
+import java.util.ArrayList;
+
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService(name="SensorRef")
+@SOAPBinding(style=Style.RPC)
+
 public class Sensor {
 
+    public long id;
     int port;
     String name;
     String existing;
     int[] directions;
+    ArrayList<SensorRef> sensorlist;
+    SensorRef coordinator;
     
     /**
      * @param port Port to run on
@@ -19,5 +31,32 @@ public class Sensor {
         this.existing = existingsensor;
         this.directions = directions;
     }
+    
+    public SensorRef[] getSensorlist(){
+        return sensorlist.toArray(new SensorRef[]{});
+    }
+    
+    // election algorithm 
+    
+    public boolean setCoordinator(SensorRef coordinator){
+        this.coordinator = coordinator;
+        return true;
+        
+    }
+    public boolean election(){
+        return true;
+    }
 
+    // Webservice functions
+    
+    public long getID(){
+        return id;
+    }
+    
+    // Koordinator functions
+  
+    public void getsomething(){
+        
+    }
+    
 }
