30e45be1d3c0c8eddd1fe0a154a1c0c6ee07e5ad
Arne Wischer <Fettlaus@gmail.com>
2011-06-19 15:47:10 +0000

    added sensor project

---
+++ Messwerterfassung/src/sensor/Sensor.java
@@ -0,0 +1,13 @@
+package sensor;
+
+public class Sensor {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
