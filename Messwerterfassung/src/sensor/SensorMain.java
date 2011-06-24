9c962e11249ec39b4011a9b4785d24a2d419a4b7
Arne Wischer <Fettlaus@gmail.com>
2011-06-24 01:49:29 +0000

    turned upside down

--- Messwerterfassung/src/sensor/SensorMain.java
+++ Messwerterfassung/src/sensor/SensorMain.java
@@ -2,68 +2,45 @@ package sensor;
 
 import javax.xml.ws.Endpoint;
 
-
 public class SensorMain {
 
-  
-	
     public static void main(String[] args) {
         String sensorName = "Sensorx";
-         String existingSensor = "";
-         String anzeige[] = null;
-         Directions directions;
-         int dirAs = 2;
-         
-         
-         if (args.length >= 1) {
-        	 sensorName = args[0];
-         }
+        String existingSensor = null;
+        String meter = null;
+        Directions directions = new Directions();
+        if (args.length < 4) {
+            System.err.println("must have at least 4 arguments");
+            return;
+        }
+        sensorName = args[0];
+        if (args[1].equalsIgnoreCase("-k")) {
+            meter = args[2];
+        } else if (args[1].equalsIgnoreCase("-s")) {
+            existingSensor = args[2];
+        }
 
-         if (args.length >= 2) {
-        	 if (args[1].equals("-k")) {
-        		 existingSensor = null;
-        	 }else if (args[1].equals("-s")){
-        		 existingSensor = args[2];
-        		 dirAs = 3;
-        	 }
-        	 
-         }
+        for (int i = 3; i < args.length; i++) {
+            if (args[i].equalsIgnoreCase("NE"))
+                directions.setNE(true);
+            if (args[i].equalsIgnoreCase("SE"))
+                directions.setSE(true);
+            if (args[i].equalsIgnoreCase("SW"))
+                directions.setSW(true);
+            if (args[i].equalsIgnoreCase("NW"))
+                directions.setNW(true);
+        }
 
-         if (args.length >= 3) {
-        	 anzeige = new String[args.length - dirAs];
-        	 for (int i = 0; i < args.length - dirAs; i++) {
-        		 anzeige[i] = args[i+dirAs];
-        	 }
-        	 
-         }
-         directions = customize(anzeige);
-         
-//         System.out.println(sensorName + " " + existingSensor);
-//     	 System.out.println(directions.isNE() + " " + directions.isSE() + " " + directions.isSW() + " " + directions.isSE());
+        // System.out.println(sensorName + " " + existingSensor);
+        // System.out.println(directions.isNE() + " " + directions.isSE() + " "
+        // + directions.isSW() + " " + directions.isSE());
 
-         SensorImpl sensor = new SensorImpl("http://localhost:9999/",sensorName, existingSensor, directions);
-         Endpoint endpoint = Endpoint.create(sensor);
-         endpoint.publish(sensorName +"sensor");
-         sensor.run();
-         endpoint.stop();
-         
-         
-         
+        SensorImpl sensor = new SensorImpl(meter, sensorName, existingSensor, directions);
+        Endpoint endpoint = Endpoint.create(sensor);
+        endpoint.publish(sensorName + "sensor");
+        sensor.run();
+        endpoint.stop();
+
     }
-    private static Directions customize(String[] anzeige){
-    	Directions directions = new Directions();
-    	for (String anz : anzeige) {
-			if(anz.equals("NE")){
-				directions.setNE(true); 
-			}else if(anz.equals("SE")){
-				directions.setSE(true); 
-			}else if(anz.equals("SW")){
-				directions.setSW(true);
-			}else if(anz.equals("NW")){
-				directions.setNW(true); 
-			}
-		}
-    	    	
-    	return directions;
-	}
+
 }
