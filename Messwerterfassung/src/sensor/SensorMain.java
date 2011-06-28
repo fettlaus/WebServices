915495bc034163dada49e95830a6c7ebd31339f0
Arne Wischer <Fettlaus@gmail.com>
2011-06-28 18:54:08 +0000

    CLOSED - task 2: plausibility checks on CMD http://github.com/fettlaus/WebServices/issues/issue/2

--- Messwerterfassung/src/sensor/SensorMain.java
+++ Messwerterfassung/src/sensor/SensorMain.java
@@ -1,5 +1,8 @@
 package sensor;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.xml.ws.Endpoint;
 
 public class SensorMain {
@@ -13,22 +16,45 @@ public class SensorMain {
             System.err.println("must have at least 4 arguments");
             return;
         }
-        sensorName = args[0];
+
+        try {
+            sensorName = args[0];
+            new URL(new URL("http://localhost:9898/"), args[0]).toString();
+        } catch (MalformedURLException e1) {
+            System.err.println("malformed location");
+            return;
+        }
         if (args[1].equalsIgnoreCase("-k")) {
             meter = args[2];
+            try {
+                new URL(new URL("http://localhost:9898/"), args[2]);
+            } catch (MalformedURLException e) {
+                System.err.println("malformed HAWMeter location");
+                return;
+            }
         } else if (args[1].equalsIgnoreCase("-s")) {
             existingSensor = args[2];
+            try {
+                new URL(new URL("http://localhost:9999/"), args[2]);
+            } catch (MalformedURLException e) {
+                System.err.println("malformed Bootstrap location");
+                return;
+            }
         }
 
         for (int i = 3; i < args.length; i++) {
-            if (args[i].equalsIgnoreCase("NE"))
+            if (args[i].equalsIgnoreCase("NE")) {
                 directions.setNE(true);
-            if (args[i].equalsIgnoreCase("SE"))
+            }
+            if (args[i].equalsIgnoreCase("SE")) {
                 directions.setSE(true);
-            if (args[i].equalsIgnoreCase("SW"))
+            }
+            if (args[i].equalsIgnoreCase("SW")) {
                 directions.setSW(true);
-            if (args[i].equalsIgnoreCase("NW"))
+            }
+            if (args[i].equalsIgnoreCase("NW")) {
                 directions.setNW(true);
+            }
         }
 
         SensorImpl sensor = new SensorImpl(meter, sensorName, existingSensor, directions);
